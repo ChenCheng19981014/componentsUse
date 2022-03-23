b9eb317abf52081b9b131579c687eab76ba2cef8
ChenCheng19981014 <cc2524261183@163.com>
2022-03-23 20:52:22 +0800

    自己学

--- src/view/tab/tab.tsx
+++ src/view/tab/tab.tsx
@@ -1,41 +1,55 @@
-import { defineComponent } from "vue";
-import until from "../component/until";
-import "./../../assets/style/tab.less";
+import { defineComponent } from 'vue';
+import until from '../component/until';
+import './../../assets/style/tab.less';
 
 // 配置页面
 const config = {
   data: {
     // 参数
     param: {
-      ls: ["属性", "场景配置", "编辑历史"],
-      com: until.menu(["属性", "场景配置", "编辑历史"]),
+      ls: ['属性', '场景配置', '编辑历史'],
+      com: until.menu(['属性', '场景配置', '编辑历史']),
     },
     // 材质
     material: {
-      ls: ["材质", "材质库"],
-      com: until.menu(["材质", "材质库"]),
+      ls: ['材质', '材质库'],
+      com: until.menu(['材质', '材质库']),
     },
     // 脚本
     script: {
-      ls: ["脚本", "脚本库", "脚本组"],
-      com: until.menu(["脚本", "脚本库", "脚本组"]),
+      ls: ['脚本', '脚本库', '脚本组'],
+      com: until.menu(['脚本', '脚本库', '脚本组']),
     },
     // 状态
     state: {
-      ls: ["状态", "状态库", "状态组"],
-      com: until.menu(["状态", "状态库", "状态组"]),
+      ls: ['状态', '状态库', '状态组'],
+      com: until.menu(['状态', '状态库', '状态组']),
     },
   },
 };
 
 // 编辑器页面
 export default defineComponent({
-  name: "tab",
+  name: 'tab',
   setup: () => () => {
     return (
       <div class="tab">
-        <div class="tab-change1">{}</div>
-        <div class="tab-change2">{}</div>
+        <div class="tab-change1 menu">
+        <p>{config.data.param.com.selectVal.value}</p>
+          {config.data.param.com.dom()}
+        </div>
+        <div class="tab-change2 menu">
+        <p>{config.data.material.com.selectVal.value}</p>
+          {config.data.material.com.dom()}
+        </div>
+        <div class="tab-change3 menu">
+        <p>{config.data.script.com.selectVal.value}</p>
+          {config.data.script.com.dom()}
+        </div>
+        <div class="tab-change4 menu">
+        <p>{config.data.state.com.selectVal.value}</p>
+          {config.data.state.com.dom()}
+        </div>
       </div>
     );
   },
